9bd7795e120efa2e08e35855b3b4a063ef8dfb24
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2024-04-02 21:26:07 +0200

    Customer_homePage_&_productPage

---
+++ src/app/(customerFacing)/products/page.tsx
@@ -0,0 +1,36 @@
+import db from "../../../db/db"
+import { Suspense } from "react"
+import { ProductCardSkeleton, ProductCard } from "@/src/components/ProductCard"
+
+function getProducts () {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+  })
+}
+
+export default function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsSuspense />
+      </Suspense>
+    </div>
+  )
+}
+
+async function ProductsSuspense() {
+  const products = await getProducts()
+
+  return products.map(product => <ProductCard key={product.id} {...product} />)
+}
